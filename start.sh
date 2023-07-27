55c909f2e5cbce54eb0c8ed7e9c74de8ae2f9bf8
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-07-27 19:38:46 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/konichiwa55115/volumeincrease /kony
+git clone https://github.com/konichiwa55115/vidsrt /kony
 cd /kony
 pip3 install -r requirements.txt
 echo "Starting Bot...."
